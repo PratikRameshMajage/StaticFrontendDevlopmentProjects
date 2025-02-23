91f688b9aaac8d76511b725919c4ec4dda17316d
Pratik Majage <89234809+PratikRameshMajage@users.noreply.github.com>
2025-02-23 23:21:55 +0530

    V25.02.23 rect moving animation

---
+++ Javascript Questions & Notes SCS ☑️/js 2/script.js
@@ -0,0 +1,8 @@
+main = document.querySelector("#main");
+main.addEventListener("mousemove", function(details){
+    var xval = gsap.utils.mapRange(0, window.innerWidth , 100 + rect.getBoundingClientRect().width/2, window.innerWidth - (rect.getBoundingClientRect().width/2), details.clientX ) 
+    gsap.to("#rect", {
+        left: xval + "px",
+        ease: Power3
+    })
+})
