318b4b45e7fa2abcb79885d3735da24a93477644
Pratik Majage <89234809+PratikRameshMajage@users.noreply.github.com>
2025-02-27 23:46:09 +0530

    V25.02.27

--- SMit ⚒️/V25.02.27/pops.js
+++ SMit ⚒️/V25.02.27/pops.js
@@ -9,8 +9,10 @@
 // alert("You Enter this Number: "+a)
 
 // It will do the Concatination. Beacouse It is Scripting Language. In input Alway's accept String.
-// var num1 = prompt("Enter 1'st Number: ")
-// var num2 = prompt("Enter 2'nd Number: ")
+var num1 = prompt("Enter 1'st Number: ")
+var num2 = prompt("Enter 2'nd Number: ")
 // var add = num1 + num2;
-// alert("Addition is : "+add)
+var add = parseInt(num1) + parseInt(num2);
+alert("Addition is : "+add)
 
+    
