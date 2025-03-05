380d4e53b56bd85cd8611da908687b75061a3471
Pratik Majage <89234809+PratikRameshMajage@users.noreply.github.com>
2025-03-05 19:35:00 +0530

    V25.03.05

---
+++ Image_Moving_Backword behind_Cursor/part1/script.js
@@ -0,0 +1,9 @@
+// var elem = document.querySelectorAll(".elem")
+// var img = document.querySelector("img")
+
+// elem.addEventListener("mouseenter", function(){
+//     img.style.left = "60%"; 
+// })
+// elem.addEventListener("mouseleave", function(){
+//     img.style.left = "20%"; 
+// })
